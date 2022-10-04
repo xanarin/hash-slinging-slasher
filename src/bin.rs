1cd08f462647578392dd8b7be7ddfe0045817365
Will Sloan <will@848.email>
2022-10-03 23:36:51 -0400

    Generalize SHA256 to take a readable object instead of bytes
    This should allow for more uses of this function, and not needing an
    insane amount of RAM to hash large datasets.
    
    Closes #1

--- src/bin.rs
+++ src/bin.rs
@@ -1,9 +1,9 @@
 use hash_slinging_slasher::sha2;
 
-
 fn main() {
-    let input = &b"hello world".to_vec();
-    let output = sha2::SHA256::from(input).hash();
-    let output_str = output.iter().map(|v| format!("{:02X}", v)).collect::<String>();
-    println!("Output Buffer:\n{}", output_str);
+    let output = sha2::SHA256::hash(&mut b"hello world".as_slice())
+        .iter()
+        .map(|v| format!("{:02X}", v))
+        .collect::<String>();
+    println!("Output Buffer:\n{}", output);
 }
